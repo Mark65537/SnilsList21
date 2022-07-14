b15e74d57a63d641169dc02090c5ae92b0a181cd
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2022-07-14 10:05:17 +0300

    изменение контроллера

--- Controllers/SnilsController.cs
+++ Controllers/SnilsController.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Collections.Generic;
+using SnilsList21.Data;
 
 namespace SnilsList21.Controllers
 {
-    public class SnilsController
+    public class SnilsController : Controller
     {
+        private readonly AppDBContext _appDBContext = new AppDBContext();
         public IActionResult Index()
         {
-            return null;
+            var model = _appDBContext.SnilsSet.ToList();
+            return View(model);
         }
     }
 }
